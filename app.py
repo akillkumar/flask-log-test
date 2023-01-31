4da7460b974d8002b756a9135e04ce8cca4aec06
Akhil Kumar <akhilf007@gmail.com>
2023-01-31 18:17:19 +0530

    using print for logging lmaoo

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 from flask import Flask
-import logging
 
 app = Flask(__name__)
 
-logging.basicConfig(filename='app.log', level=logging.INFO)
-
 @app.route('/')
 def hello():
-    logging.info('Request made to the root endpoint.')
+    print('Request made to the root endpoint.')
     return 'Hello World!'
 
 @app.route('/warn')
 def line1():
-    logging.info('WARN: here is a test warning')
+    print('WARN: here is a test warning')
     return 'Warning written to logs..'
 
 @app.route('/err')
 def line2():
-    logging.info('ERR: Oh no you have an error sadge :(')
+    print('ERR: Oh no you have an error sadge :(')
     return 'Error written to logs..'
 
 @app.route('/log')
 def line3():
-    logging.info('Good log')
+    print('Good log')
     return 'Default written to logs..'
 
 if __name__ == '__main__':
