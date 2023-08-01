143bd33340df28572b324f033e000ec9703f0c23
Akhil Kumar <akhilf007@gmail.com>
2023-08-01 12:12:56 -0700

    revert to logging module

--- app.py
+++ app.py
@@ -10,20 +10,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    #logging.info('Request made to the root endpoint.')
-    print('Request made to the root endpoint.')
+    logging.info('Request made to the root endpoint.')
     return 'Hello World! \n Available paths: /warn | /err | /count'
 
 @app.route('/warn')
 def line1():
-    #logging.warning('Here is a test warning')
-    print('Here is a test warning')
+    logging.warning('Here is a test warning')
     return 'Warning written to logs..'
 
 @app.route('/err')
 def line2():
-    #logging.error('Oh no you have an error sadge :(')
-    print('Oh no you have an error sadge :(')
+    logging.error('Oh no you have an error sadge :(')
     return 'Error written to logs..'
 
 @app.route('/count')
